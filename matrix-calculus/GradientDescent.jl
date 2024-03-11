502e429d90845e31fd89705418e4a5949f275f23
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-03-12 00:27:37 +0100

    Visualize linear system

--- matrix-calculus/GradientDescent.jl
+++ matrix-calculus/GradientDescent.jl
@@ -5,33 +5,36 @@ using Printf
 function opt(initial_params, update_fn, err_fn, x_fn; n_iterations = 1000)
     errors = []
     xs = []
-    grads = []
+    params = []
 
     Pᵢ = initial_params
     for idx in 1:n_iterations
-        new_P, gradᵢ = update_fn(Pᵢ, grads)
-        push!(grads, gradᵢ)
-        grads = grads[max(1, end-100):end]
+        new_P, _ = update_fn(Pᵢ, [])
 
         if isapprox(new_P, Pᵢ, atol=1e-4)
             println("Iteration $idx. Update did not change parameters. Exiting opt()")
-            return errors, xs
-        end        
+            break
+        end       
+ 
         Pᵢ = new_P
+        errᵢ = err_fn(Pᵢ)
+        xᵢ = x_fn(Pᵢ)
+        push!(errors, errᵢ)
+        push!(params, Pᵢ)
+        push!(xs, xᵢ)
 
-        if idx % 10 == 0
-            errᵢ = err_fn(Pᵢ)
-            xᵢ = x_fn(Pᵢ)
-            push!(errors, errᵢ)
-            push!(xs, xᵢ)
+        if isapprox(errᵢ.val, 0.0, atol=1e-4)
+            println("Iteration $idx. Error is close to zero. Exiting opt()")
+            break
         end
+
         if idx % 100 == 0
             println("Iteration $idx. x=", [round(x.val, digits=3) for x in xᵢ])
         end
     end
 
     errors = [e.val for e in errors] # convert to Float64
-    return errors, xs
+    return errors, xs, params
 end
 
 end
