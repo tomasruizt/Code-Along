e68ac9db362527fef5346a2bd851612f3d4fce16
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-09-27 19:41:15 +0200

    Introduce CUDA code

--- cuda/vector_mult.c
+++ cuda/vector_mult.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <cuda.h>
 #include "operations.h"
 
 int main() {
@@ -7,6 +8,18 @@ int main() {
     int b[] = {1, 2, 3, 4, 5};
     int c[n];
     mult(a, b, c, n);
+
+    // Device pointers
+    int *d_a, *d_b, *d_c;
+
+    // Allocate memory on the device
+    cudaMalloc((void**)&d_a, n * sizeof(int));
+    cudaMalloc((void**)&d_b, n * sizeof(int));
+    cudaMalloc((void**)&d_c, n * sizeof(int));
+
+    // Copy data from host to device
+    cudaMemcpy(d_a, a, n * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, n * sizeof(int), cudaMemcpyHostToDevice);
 
     printf("a = ");
     print_vector(a, n);
