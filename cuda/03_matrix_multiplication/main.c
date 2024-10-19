27f2c369627029f2051c00c4330ec25f48d14fb5
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-10-19 14:25:51 +0200

    Use armadillo to get matrices

---
+++ cuda/03_matrix_multiplication/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <armadillo>
+
+int idx(int i, int j, int rowSize) {
+    return i * rowSize + j;
+}
+
+void print_matrix(int m, int n, double *A) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f ", A[idx(i, j, n)]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int m = 10;
+    int n = 10;
+    int p = 10;
+    arma::mat A(m, n, arma::fill::randu);  // 1000x1000 random matrix
+    arma::mat B(n, p, arma::fill::randu);  // 1000x1000 random matrix
+    arma::mat C = A * B;  // Matrix multiplication
+    print_matrix(m, p, C.memptr());
+    return 0;
+}
