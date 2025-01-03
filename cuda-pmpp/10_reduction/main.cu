e4f8a0c5e308a7ae440460cddc1198b1ce79c321
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-01-03 15:19:23 +0100

    Update naive reduction code on cuda

--- cuda-pmpp/10_reduction/main.cu
+++ cuda-pmpp/10_reduction/main.cu
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include "main.h"
 
-int main() {
-    int n = 1024 * 2;
-    float* vec = arange(n);
-    float sum = cpu_sum(vec, n);
-    printf("CPU sum: %.2f\n", sum);
-
-    float* sum_h = cuda_sum(vec, n);
-    printf("CUDA sum: %.2f\n", sum_h[0]);
-    return 0;
+__global__ void cudaNaiveSumKernel(float* vec, int len, float* result) {
+    unsigned int tx = threadIdx.x;  // Apparently the unsigned int is required
+    // The particular stride creates uncoalesced memory access
+    for (unsigned int stride = 1; stride <= blockDim.x; stride *= 2) {
+        if (tx % stride == 0)
+            vec[2 * tx] += vec[2 * tx + stride];
+        __syncthreads();
+    }
+    if (tx == 0)
+        result[0] = vec[0];
 }
 
-float* cuda_sum(float *vec, int len)
+float cudaNaiveSum(float *vec, int len)
 {
     float *sum_h = new float[1];
     float *sum_d, *vec_d;
@@ -22,7 +22,7 @@ float* cuda_sum(float *vec, int len)
 
     dim3 threadsPerBlock(len / 2);
     dim3 numBlocks(1);
-    cudaNaiveSum<<<numBlocks, threadsPerBlock>>>(vec_d, len, sum_d);
+    cudaNaiveSumKernel<<<numBlocks, threadsPerBlock>>>(vec_d, len, sum_d);
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) {
         printf("CUDA error: %s\n", cudaGetErrorString(err));
@@ -31,7 +31,7 @@ float* cuda_sum(float *vec, int len)
     cudaMemcpy(sum_h, sum_d, sizeof(float), cudaMemcpyDeviceToHost);
     cudaFree(sum_d);
     cudaFree(vec_d);
-    return sum_h;
+    return sum_h[0];
 }
 
 float* arange(int n)
@@ -52,12 +52,13 @@ float cpu_sum(float* vec, int len) {
     return sum;
 }
 
-__global__ void cudaNaiveSum(float* vec, int len, float* result) {
-    unsigned int tx = threadIdx.x;  // Apparently the unsigned int is required
-    for (unsigned int stride = 1; stride <= blockDim.x; stride *= 2) {
-        if (tx % stride == 0)
-            vec[2 * tx] += vec[2 * tx + stride];
-    }
-    if (tx == 0)
-        result[0] = vec[0];
+int main() {
+    int n = 1024 * 2;
+    float* vec = arange(n);
+    float sum = cpu_sum(vec, n);
+    printf("CPU sum: %.2f\n", sum);
+
+    float sum_h = cudaNaiveSum(vec, n);
+    printf("CUDA sum: %.2f\n", sum_h);
+    return 0;
 }
