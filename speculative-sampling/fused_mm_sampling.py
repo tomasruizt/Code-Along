5c499fd0edf83a7195a80d1cb3e086c61b2eb41b
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-10-29 17:22:48 +0100

    Update helion kernel with torch.rand_like()

--- speculative-sampling/fused_mm_sampling.py
+++ speculative-sampling/fused_mm_sampling.py
@@ -192,44 +192,37 @@ def fused_sample_triton_kernel(
 
 
 @helion.kernel(autotune_effort="none")
-def matmul(weights: torch.Tensor, hidden_states: torch.Tensor) -> torch.Tensor:
-    """
-    Currently, the torch.rand() call fails, because it does not resolve the
-    symbolic dimensions correctly. I reported the bug here:
-    https://github.com/pytorch/helion/issues/1041
-    """
+def fused_sample_helion(
+    weights: torch.Tensor, hidden_states: torch.Tensor
+) -> torch.Tensor:
     assert weights.size(1) == hidden_states.size(0)
     V, D = weights.size()
     seq_len = hidden_states.size(1)
     hl_seq_len = hl.specialize(seq_len)
 
-    num_samples = 2
-    gumbel_max = float("-inf") * torch.ones(
-        size=(num_samples, seq_len), device=weights.device
-    )
-    gumbel_max_idx = torch.empty(
-        size=(num_samples, seq_len), dtype=torch.long, device=weights.device
-    )
+    gumbel_max = float("-inf") * torch.ones(seq_len, device=weights.device)
+    gumbel_max_idx = torch.empty(seq_len, dtype=torch.long, device=weights.device)
 
     for tile_v in hl.tile(V):
         logits_blk = hl.zeros([tile_v, hl_seq_len], dtype=torch.float32)
         for tile_d in hl.tile(D):
             mm = torch.matmul(weights[tile_v, tile_d], hidden_states[tile_d, :])
             logits_blk = logits_blk + mm
-        unif_noise = torch.rand(
-            [num_samples, tile_v.block_size, hl_seq_len], dtype=torch.float32
-        )
+        # We cannot use torch.rand here to generate multiple samples at once,
+        # because it is not yet supported by Helion.
+        # https://github.com/pytorch/helion/issues/1041
+        unif_noise = torch.rand_like(logits_blk, dtype=torch.float32)
         gumbel_noise = -(-unif_noise.log()).log()
         # [num_samples, seq_len]
         summed = logits_blk + gumbel_noise
-        new_max = hl.reduce(torch.max, summed, dim=1)
-        new_max_idx_local = torch.argmax(summed, dim=1)
+        new_max = hl.reduce(torch.max, summed, dim=0)
+        new_max_idx_local = torch.argmax(summed, dim=0)
         new_max_idx_global = tile_v.begin + new_max_idx_local
 
-        replace_mask = new_max > gumbel_max[:, :]
-        gumbel_max[:, :] = torch.where(replace_mask, new_max, gumbel_max[:, :])
-        gumbel_max_idx[:, :] = torch.where(
-            replace_mask, new_max_idx_global, gumbel_max_idx[:, :]
+        replace_mask = new_max > gumbel_max[:]
+        gumbel_max[:] = torch.where(replace_mask, new_max, gumbel_max[:])
+        gumbel_max_idx[:] = torch.where(
+            replace_mask, new_max_idx_global, gumbel_max_idx[:]
         )
 
     return gumbel_max, gumbel_max_idx
